81fd48ad10c497054cf8ad10ab7b2c4e48acc13a
Roger Collins <zulrang@gmail.com>
2015-10-07 13:33:32 -0400

    Create weighted-compressed-quick-union.c

---
+++ union-find/weighted-compressed-quick-union.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define N 10000
+
+int main() {
+	int id[N], sz[N], i, j, p, q;
+	
+	// initialize array
+	for(i=0; i<N; i++) { id[i] = i; sz[i] = 1; }
+
+	while(scanf("%d %d\n", &p, &q) == 2) {
+		// get the root node of each input
+		for(i = p; i != id[i]; i = id[i]) id[i] = id[id[i]];
+		for(j = q; j != id[j]; j = id[j]) id[j] = id[id[j]];
+		if (i == j) continue; // same root node, already connected
+
+		// different root node, set them to be the same weighted by size
+		if (sz[i] < sz[j]) {
+			id[i] = j; // set root of i to j
+			sz[j] += sz[i]; // add size of i to j
+		} else {
+			id[j] = i; // set root of j to i
+			sz[i] += sz[j]; // add size of j to i
+		}
+		printf(" %d %d", p, q);
+		printf("\n");
+	}
+
+	return 0;
+}
